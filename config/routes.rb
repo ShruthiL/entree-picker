12890956253fcc0dadb519f80858aa2554cdca28
Shruthi <sruthiraj.lagisetty@gmail.com>
2020-05-13 20:42:32 -0400

    added change theme logic

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :change_themes, only: [:index, :create]
       resources :picked_entrees, only: [:index, :create, :update, :destroy]
       resources :reviews, only: [:edit, :create, :update, :destroy]
       resources :site_reviews, only: [:index, :create, :update, :destroy]
