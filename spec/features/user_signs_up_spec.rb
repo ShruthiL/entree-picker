5f06ee0dea5eba2e7df9efaf53aca3340b98077e
Shruthi <sruthiraj.lagisetty@gmail.com>
2020-05-16 22:29:28 -0400

    updates tests

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -19,8 +19,6 @@ feature 'user registers', %Q{
     fill_in 'Last Name', with: "Last Name"
     fill_in 'User Name', with: "User Name"
     fill_in 'Email', with: 'john@example.com'
-    fill_in 'Phone Number', with: 123456789
-    fill_in 'Zip Code', with: 12345
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
 
